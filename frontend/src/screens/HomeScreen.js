fdbdc2fdf9c73853a1634c07781fd5df454d7ee0
Cvijetin Mladjenovic <mladjenovic.cvijetin@gmail.com>
2021-10-31 07:06:23 -0700

    Integrated react-redux with current project

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
 
   // get's triggered when the component reloads or state value changes
   useEffect(() => {
-    async function fetchProducts() {
-      const { data } = await axios.get("/api/products/");
-      setProducts(data);
-    }
-
-    fetchProducts();
-  }, []); // empty [] means that we want this to tirgger only when the component loads not when the state changes
+    dispatch(listProducts());
+  }, [dispatch]); // empty [] means that we want this to tirgger only when the component loads not when the state changes
 
   return (
     <div>
       <h1>Latest products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}></Product>
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader></Loader>
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product}></Product>
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 }
