d648df57e1185b685bd4a27c4b617ae263b1cd5e
Cvijetin Mladjenovic <mladjenovic.cvijetin@gmail.com>
2021-10-31 15:07:13 -0700

    Implemented cart functionality
    Implemented cart functionality with redux integration

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { cartReducer } from "./reducers/cartReducers";
+
 import {
   productListReducer,
   productDetailsReducer,
@@ -9,9 +11,16 @@ import {
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
